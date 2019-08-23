538988fe322990a4bc7463ca1baf52e8e3c89109
natnatparis <natnatparis1@gmail.com>
2019-08-23 02:35:33 +0200

    ex 4

--- stylesheet/js/script_3.js
+++ stylesheet/js/script_3.js
@@ -2,9 +2,11 @@
 
   var i, j;
   var n=prompt("Enter a number for the no. of rows in a pattern");
+  console.log("Enter a number for the no. of rows in a pattern")
   //outer loop
   for(i=1; i <= n; i++)
    {
+    console.log(" is the numbers of stairs for the pyramid")
    //inner loop
     for(j=1; j<=i; j++)
    {
@@ -12,4 +14,5 @@
     }
      document.write('<br/>');
    }
+   console.log()
 
--- stylesheet/js/script_4.js
+++ stylesheet/js/script_4.js
@@ -17,7 +17,56 @@ const entrepreneurs = [
     { first: 'Travis', last: 'Kalanick', year: 1976 },
     { first: 'Marc', last: 'Andreessen', year: 1971 },
     { first: 'Peter', last: 'Thiel', year: 1967 }
-  ];
+  ]
 
- entrepreneurs["first", "last"] ;
- console.log(entrepreneurs)
+  var prenom_nom = [];
+
+  // patron des 70's
+  function seventies(patron) {
+      let i = 0;
+      let tab = [];
+  
+      console.log("les entrepreneurs nes dans les 70's sont: ")
+      while (i < patron.length) {
+          Object.entries(patron[i]).forEach(function([key, value]) {
+              if (key == "year" && (value <= 1979 && value >= 1970)) {
+                  console.log(patron[i].first, " ", patron[i].last);
+                  tab.push(patron[i].first + " " + patron[i].last);
+              }
+          });
+      i++;
+      }
+  
+      return tab;
+  }
+  // liste des noms et prenoms
+  function prenom_nom_in_tab(patron) {
+    
+      let i = 0;
+      
+      console.log( "Voici la liste des noms et prenoms")
+      while (i < patron.length) {
+          prenom_nom.push(patron[i].first + " " + patron[i].last)
+          i++;
+      }
+  }
+  // calcul de l age
+  function age_of_patron(patron) {
+      let i = 0;
+      
+      while (i < patron.length) {
+          console.log("Aujourd'hui", patron[i].first, patron[i].last, "as", 2019 - patron[i].year, "ans")
+          i++;
+      }
+  }
+  // tri alphabetique
+  function sort_tab(entrepreneurs) {
+      return entrepreneurs.sort((a, b) => a.last.localeCompare(b.last));
+  }
+  
+  seventies(entrepreneurs);
+  prenom_nom_in_tab(entrepreneurs);
+  console.log(prenom_nom);
+  age_of_patron(entrepreneurs);
+  console.log("voici la liste des entrepreneurs dans l'ordre alphabetique\n", sort_tab(entrepreneurs));
+  
